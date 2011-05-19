0680b49ebb0d1ebdd77469808ac5bfab5d1ca261
Luke Amdor <luke.amdor@t8webware.com>
2011-05-19 14:45:36 -0500

    Updating some deps (hoped to move to 2.9.0, but no go yet)

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -10,7 +10,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ApacheLicense
   // necessary for scalaz snapshot that specs2 depends on
   val snapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
 
-  val specs2 = "org.specs2" %% "specs2" % "1.1" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.3" % "test"
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 
