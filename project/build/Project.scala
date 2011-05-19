03830d0f204a3534405e9bd027e8abd0797899b2
Luke Amdor <luke.amdor@t8webware.com>
2011-05-19 14:36:00 -0500

    A release can't have snapshot deps

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,7 +4,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ApacheLicense
   val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
   val novusSnaps = "repo.novus snaps" at "http://repo.novus.com/snapshots/"
 
-  val salat = "com.novus" %% "salat-core" % "0.0.7-SNAPSHOT"
+  val salat = "com.novus" %% "salat-core" % "0.0.7"
   val avro = "org.apache.avro" % "avro" % "1.5.0"
 
   // necessary for scalaz snapshot that specs2 depends on
@@ -15,7 +15,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ApacheLicense
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 
   Credentials(Path.userHome / ".ivy2" / ".banno_credentials", log)
-  lazy val publishTo = "Banno Snapshots Repo" at "http://10.3.0.26:8081/nexus/content/repositories/snapshots"
+  lazy val publishTo = "Banno Snapshots Repo" at "http://10.3.0.26:8081/nexus/content/repositories/releases"
 
   def copyrightLine = "Copyright 2011 T8 Webware"
   override def compileAction = super.compileAction dependsOn formatLicenseHeaders
