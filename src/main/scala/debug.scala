c634cea1c280f20c4a6d44702b94098d218ef930
Julian Peeters <julianpeeters@gmail.com>
2013-06-24 11:04:41 -0700

    what a mess

--- src/main/scala/debug.scala
+++ src/main/scala/debug.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-
+package debug
 import models._
 import com.banno.salat.avro._
 import com.banno.salat.avro.global._
@@ -23,7 +23,7 @@ import java.io.File
 //import reflect.ScalaSignature
 //import reflect.generic.ByteCodecs
 //import scala.tools.scalap.scalax.rules.scalasig._
-//import org.objectweb.asm._
+import org.objectweb.asm._
 import org.apache.avro._
 import org.apache.avro.io._
 import org.apache.avro.file._
@@ -32,29 +32,31 @@ import org.apache.avro.generic._
 import scala.util.parsing.json._
 
 object Main extends App {
+//  val myRecord1 = MyRecord("Hi", 2, true)
+  val myRecord1 = MyRecord(rec(), 2, true)
+  //val myRecord1 = MyRecord(List("pantagruel", "gargantua"), List(1,"hi"), List(List(List("1innermost", "1nest"), List("2innermost", "2nest")), List(List("1mid", "1mnest"), List("2mid", "2mnest"))))
+  //val myRecord2 = MyRecord(List())
+  //val myRecord3 = MyRecord("Escher")
 
-//  val myRecord1 = MyRecord("Tortoise")
-//  val myRecord2 = MyRecord("Achilles")
-//  val myRecord3 = MyRecord("Escher")
-
- // val myRecordIterator = Iterator[MyRecord](myRecord1, myRecord2, myRecord3)
+  val myRecordIterator = Iterator[MyRecord](myRecord1)//, myRecord2, myRecord3)
   //lazy val myRecordIterator = Iterator[models](myRecord1, myRecord2, myRecord3)
 /*
 In this example, we'll need to show that both pre-serialized and deserialized records are equal.  But normally the iterator is consumed upon serialation, leaving us nothing to compare to the deserialized result.  To solve this, we'll make a copy of the iterator, serializing myRecordIterator1 and leaving myRecordIterator2 for use in comparison.  A `Stream` can always be had from an `Iterator` if immutability is desired, at the possible risk of memory issues for large files(?).
 */
 
- // val(myRecordIterator1, myRecordIterator2) = myRecordIterator.duplicate
+  val(myRecordIterator1, myRecordIterator2) = myRecordIterator.duplicate
 
 /*-------------STREAM RECORDS TO AND FROM AVRO DATAFILE------------------------------------------
 In order to stream records to an avro file that can be read by an avro datafilereader, we need to provide record model, a destination file, and a stream of records. `serializeIteratorToDataFile` appends indiscriminately.  If there is no file, a file is created.  To deserialize from file we will need to provide a record model and an infile. 
 */
 
-/*
-//Serialize to an Avro DataFile: Record model MyRecord is defined in package models
-  val outfile = new File("/home/julianpeeters/salat-avro-example/data/input.avro")
-  grater[MyRecord].serializeCollectionToFile(outfile, myRecordIterator1)
-*/
 
+//Serialize to an Avro DataFile: Record model MyRecord is defined in package models
+  val outfile = new File("input.avro")
+  grater[MyRecord].serializeCollectionToFile(outfile, myRecordIterator1)
+
+
+/*
 //Deserialize from File: Read DataFile, dynamically create the model class and deserialize back to object 
   val infile = new File("input.avro")
 
@@ -97,6 +99,8 @@ In order to stream records to an avro file that can be read by an avro datafiler
   println(namespace)
   println(caseclass)
   println(fields) 
+val desc = Type.getDescriptor(classOf[String])
+println(desc)
 
  /*
  val result = json match {
@@ -146,5 +150,5 @@ In order to stream records to an avro file that can be read by an avro datafiler
  // println("All Records From Avro Data File Same As The Originals?: " + (sameRecordIterator sameElements myRecordIterator2))
 
 */
-
+*/
 }
